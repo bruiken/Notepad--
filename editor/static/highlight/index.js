25d4444f783e938a5359b9bc2f62843cb583d101
Huy Nguyen <huy.nguy97@gmail.com>
2020-11-23 11:39:43 +0100

    buttons fixed

--- editor/static/highlight/index.js
+++ editor/static/highlight/index.js
@@ -52,3 +52,34 @@ function processPost(textarea) {
     });
     isRequesting = true;
 }
+
+function selectedText(){
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function search(site) {
+    switch (site){
+        case 'google': 
+            window.open("http://www.google.com/search?q=" + selectedText());
+            break;
+        case 'youtube':
+            window.open("https://www.youtube.com/results?search_query=" + selectedText());
+            break;
+        case 'stackoverflow':
+            window.open("https://stackoverflow.com/search?q=" + selectedText());
+            break; 
+        case 'reddit':
+            window.open
+            ("https://www.reddit.com/r/ProgrammerHumor/search?q=" + selectedText() + 
+            "&restrict_sr=on&sort=relevance&t=all")
+        default: 
+            console.alert("No site found")
+        
+    }
+}
