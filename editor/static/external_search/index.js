d390694162c577891310fb41bb96babf4d68a91b
Huy Nguyen <huy.nguy97@gmail.com>
2020-11-26 12:03:32 +0100

    simplified the selectedText() code

--- editor/static/external_search/index.js
+++ editor/static/external_search/index.js
@@ -1,10 +1,6 @@
 function selectedText(){
     var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
+    text = window.getSelection().toString();
     return text;
 }
 
