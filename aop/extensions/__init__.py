1b71d0b22f603f4624727e1d825992d977b832dc
jwijenbergh <jeroenwijenbergh@protonmail.com>
2020-11-27 15:16:00 +0100

    Add tabs to aop

--- aop/extensions/__init__.py
+++ aop/extensions/__init__.py
@@ -1,6 +1,8 @@
 from importlib import import_module
 
-possible_features = ['aop.extensions.highlight',
+possible_features = [
+                     'aop.extensions.tabs',
+                     'aop.extensions.highlight',
                      'aop.extensions.linenumbers',
                      'aop.extensions.functionclick',
                      'aop.extensions.externalsearch',
