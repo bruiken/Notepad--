95e3d0f7a9414701a0c7d3da5522f3ab20a7361d
Huy Nguyen <huy.nguy97@gmail.com>
2020-11-30 15:55:11 +0100

    fixed firefox hardcoded download folder and gitignore

--- aop/testing/main.py
+++ aop/testing/main.py
@@ -10,7 +10,7 @@ load_dotenv()
 if bool(strtobool(os.getenv("FIREFOX"))):
     fp = webdriver.FirefoxProfile()
     fp.set_preference("browser.download.folderList",2)
-    fp.set_preference("browser.download.dir", "C:\\Users\\Huy\\Downloads")
+    fp.set_preference("browser.download.dir", os.getenv("TEST_DOWNLOAD_FOLDER"))
     fp.set_preference("browser.download.manager.showWhenStarting", False)
     fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
     browser = webdriver.Firefox(executable_path=os.getenv("WEBDRIVER_FIREFOX"),firefox_profile=fp)
