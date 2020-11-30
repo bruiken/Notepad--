adb73bc8c709458cde97f76e82fd0a5f638e537c
Bob Ruiken <bobruiken@gmail.com>
2020-11-30 15:34:39 +0100

    Use new functions for search and replace

--- editor/static/search_and_replace/index.js
+++ editor/static/search_and_replace/index.js
@@ -1,9 +1,6 @@
 function searchLocal(search, replace){
     var searchTerm = search.value; 
     var replaceTerm = replace.value;
-    var textArea = document.getElementById('textArea'); // current tab text
-    var toReplace = textArea.value;
-    var newText = toReplace.replaceAll(searchTerm, replaceTerm)
-    localStorage.setItem(textArea.id,newText);
-    location.reload();
+    var newText = getEditorText().replaceAll(searchTerm, replaceTerm)
+    setEditorText(newText);
 } 
