a40ad295415a3921889faea6b21e7d9e3bdada9e
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-02 18:48:17 +0100

    sharing buttons fixed and added some more

---
+++ editor/static/sharing/index.js
@@ -0,0 +1,12 @@
+function share(program){
+    if (program =="whatsapp"){
+        var whatsapp = "https://web.whatsapp.com/send?text=```";
+        var text = savedTabInfo[focusedTab].text;
+        console.log(text);
+        window.open(whatsapp + text + "```" + "%0a%0a This was shared via Notepad--");
+    }
+    else if (program=="mail")
+        window.open('mailto:test@example.com?subject=This was shared via Notepad--&body=' +savedTabInfo[focusedTab].text);
+    else if (program=="reddit")
+        window.open('https://www.reddit.com/r/ProgrammerHumor/submit?selftext=true')
+}
