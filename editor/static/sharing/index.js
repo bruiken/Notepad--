aeb95489129523279c7edae9ca7f66ac84c91b25
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-03 12:06:17 +0100

    fixed mail newlines

--- editor/static/sharing/index.js
+++ editor/static/sharing/index.js
@@ -6,7 +6,7 @@ function share(program){
         window.open(whatsapp + text + "```" + "%0a%0a This was shared via Notepad--");
     }
     else if (program=="mail")
-        window.open('mailto:test@example.com?subject=This was shared via Notepad--&body=' +savedTabInfo[focusedTab].text);
+        window.open('mailto:test@example.com?subject=This was shared via Notepad--&body=' +  encodeURIComponent(savedTabInfo[focusedTab].text));
     else if (program=="reddit")
         window.open('https://www.reddit.com/r/ProgrammerHumor/submit?selftext=true')
 }
