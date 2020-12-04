cd6935031d300ce5ddc8cff0e1d02c0199471056
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-05 00:32:09 +0100

    fixed more bugs comparing diff between tabs with new unsaved data

--- editor/static/diff/index.js
+++ editor/static/diff/index.js
@@ -1,32 +1,9 @@
 function diffPost(){  
-    const object = tabInfo;
-    var keyArray = [];
-    for (const [key, value] of Object.entries(object))
-        keyArray.push(key);
+    var keyArray = setupKeyArray();
     var input = document.getElementById("placeholderDiff").value;
-    var valid = true;
-    var base = ""
-    var toCompare = ""
-    if (input == ""){ // default case
-        if (focusedTab == 0){
-            base = getEditorText();
-            toCompare = savedTabInfo[keyArray[1]].text;
-        }
-        else{
-            base = tabInfo[0].text;
-            toCompare = savedTabInfo[keyArray[1]].text;
-        }
-    }
-    else if (input > 0 && input <= totalTabs){
-        base = getEditorText();
-        toCompare = tabInfo[keyArray[input-1]].text;
-    }
-    else {
-        alert("Invalid input");
-        valid = false;
-    }
-
-        fetch('/diff', {
+    var base = processBaseInput(input, keyArray);
+    var toCompare = processCompareInput(input, keyArray);
+    fetch('/diff', {
         headers: {
           'Content-Type': 'application/json'
         },
@@ -38,13 +15,50 @@ function diffPost(){
     }).then(function (response) { 
         return response.text();
     }).then(function (text) {
-        if (valid)
-            showDiff(text)
+        if (base != false && toCompare != false)
+            showDiff(text);
+        else 
+            alert("Invalid input");
     });
-
 }
 
+function setupKeyArray(){
+    const object = tabInfo;
+    var keyArray = [];
+    for (const [key, value] of Object.entries(object))
+        keyArray.push(key);
+    return keyArray;
+}
 
+function processCompareInput(input, keyArray){
+    var toCompare = "";
+    if (input == "")
+        if (focusedTab != 0 && focusedTab == keyArray[1])
+            toCompare = getEditorText(); 
+        else 
+            toCompare = tabInfo[keyArray[1]].text;
+    else if (input > 0 && input <= totalTabs)
+        toCompare = tabInfo[keyArray[input-1]].text;
+    else 
+        return false;
+    return toCompare
+}
+
+function processBaseInput(input, keyArray){
+    var base = "";
+    console.log("input ", input);
+    if (input == ""){ 
+        if (focusedTab == 0)
+            base = getEditorText();
+        else
+            base = tabInfo[0].text; 
+    }
+    else if (input > 0 && input <= totalTabs)
+        base = getEditorText();
+    else 
+        return false;
+    return base;
+}
 
 function showDiff(diff){
     var diffWindow = window.open("");
