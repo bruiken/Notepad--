fe743721bebe9d79b23aab288db969dbe9c737da
jwijenbergh <jeroenwijenbergh@protonmail.com>
2020-12-07 14:42:25 +0100

    Rewrote getting diffs with tabs and can now use it without tabs

--- editor/static/diff/index.js
+++ editor/static/diff/index.js
@@ -1,25 +1,67 @@
 function diffPost(){  
-    var keyArray = setupKeyArray();
     var input = document.getElementById("placeholderDiff").value;
-    var base = processBaseInput(input);
-    var toCompare = processCompareInput(input, keyArray);
+    var diffs = ["", ""];
+    if(typeof focusedTab !== 'undefined') {
+	var keyArray = setupKeyArray();
+	diffs = getDiffTextTabs(input, keyArray);
+    } else {
+	diffs = getDiffText();
+    }
+    if(diffs === null) {
+	alert("Invalid input");
+	return;
+    }
     fetch('/diff', {
         headers: {
           'Content-Type': 'application/json'
         },
         method: 'POST',
         body: JSON.stringify({
-            "one": base,
-            "two": toCompare
+            "one": diffs[1],
+            "two": diffs[0]
         })
     }).then(function (response) { 
         return response.text();
     }).then(function (text) {
-        if (base !== false && toCompare !== false)
-            showDiff(text);
-        else 
-            alert("Invalid input");
+        showDiff(text);
     });
+}
+
+function getDiffTextTabs(input, keyArray) {
+    // Default
+    var firstTab = 0;
+    var secondTab = 1;
+
+    // Compare with itself if there is only one tab
+    if(totalTabs <= 1) {
+	secondTab = 0;
+    }
+
+    // If the user has made a choice
+    if(input !== "") {
+	// The input is valid
+	if(input > 0 && input <= keyArray.length) {
+	    firstTab = focusedTab;
+	    secondTab = keyArray[input-1];
+	} else {
+	    return null;
+	}
+    }
+
+    if(firstTab === secondTab) {
+	return [getTextById(firstTab), savedTabInfo(secondTab)];
+    }
+
+    return [getTextById(firstTab), getTextById(secondTab)];
+}
+
+function getDiffText() {
+    return [getEditorText(), savedText];
+}
+
+function getTextById(id) {
+    if (id === focusedTab) return getEditorText();
+    return tabInfo[id].text;
 }
 
 function setupKeyArray(){
@@ -28,37 +70,6 @@ function setupKeyArray(){
     for (const [key, value] of Object.entries(object))
         keyArray.push(key);
     return keyArray;
-}
-
-function processCompareInput(input, keyArray){
-    var toCompare = "";
-    if (input == "")
-        if (focusedTab != 0 && focusedTab == keyArray[1])
-            toCompare = getEditorText(); 
-        else 
-            toCompare = tabInfo[keyArray[1]].text;
-    else if (input > 0 && input <= totalTabs)
-        toCompare = tabInfo[keyArray[input-1]].text;
-    else {
-        return false;
-    }
-
-    return toCompare
-}
-
-function processBaseInput(input){
-    var base = "";
-    if (input == ""){ 
-        if (focusedTab == 0)
-            base = getEditorText();
-        else
-            base = tabInfo[0].text; 
-    }
-    else if (input > 0 && input <= totalTabs)
-        base = getEditorText();
-    else 
-        return false;
-    return base;
 }
 
 function showDiff(diff){
