5aced248efb3dce6afd03e1d5443c08898f9e3cd
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-07 13:53:30 +0100

    mail uses geteditortext as well

--- editor/static/sharing/index.js
+++ editor/static/sharing/index.js
@@ -5,7 +5,7 @@ function share(program){
         window.open(whatsapp + encodeURIComponent(text) + "```" + "%0a%0a This was shared via Notepad--");
     }
     else if (program=="mail")
-        window.open('mailto:test@example.com?subject=This was shared via Notepad--&body=' +  encodeURIComponent(savedTabInfo[focusedTab].text));
+        window.open('mailto:test@example.com?subject=This was shared via Notepad--&body=' +  encodeURIComponent(getEditorText()));
     else if (program=="reddit")
         window.open('https://www.reddit.com/r/ProgrammerHumor/submit?selftext=true')
 }
