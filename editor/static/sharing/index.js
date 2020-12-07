363e18b5f3ce7360d32da5e004f2f4bdd6d67349
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-07 13:48:29 +0100

    removed tweet button and console.log

--- editor/static/sharing/index.js
+++ editor/static/sharing/index.js
@@ -2,7 +2,6 @@ function share(program){
     if (program =="whatsapp"){
         var whatsapp = "https://web.whatsapp.com/send?text=```";
         var text = getEditorText();
-        console.log(text);
         window.open(whatsapp + encodeURIComponent(text) + "```" + "%0a%0a This was shared via Notepad--");
     }
     else if (program=="mail")
