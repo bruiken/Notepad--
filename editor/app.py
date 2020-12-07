7472ccd43bbf3d801a4a4803b0228d39343a8b3a
Huy Nguyen <huy.nguy97@gmail.com>
2020-12-07 15:31:10 +0100

    import jsonify removed

--- editor/app.py
+++ editor/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 import os
 import aop
